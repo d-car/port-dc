0f30c440fa11dc0b257add3ea6cc278397754e14
unknown <danielscarstensen@gmail.com>
2019-03-28 12:29:29 -0500

    working on mobile

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,8 @@
-const pboxOverlay = document.getElementById("pbox-overlay");
-const pbox1 = document.getElementById("pbox1pic");
+// const pboxOverlay = document.getElementById("pbox-overlay");
+// const pbox1 = document.getElementById("pbox1pic");
 
-pbox1.addEventListener("mouseover", function () {
-    console.log("mouseover")
-    event.document.getElementById("pbox-overlay").setAttribute("style", "opacity: 0.5");
+// pbox1.addEventListener("mouseover", function () {
+//     console.log("mouseover")
+//     event.document.getElementById("pbox-overlay").setAttribute("style", "opacity: 0.5");
 
-})
+// })
